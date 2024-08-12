01923f5c007c1c9b584e2d0adfe7d28bd412b144
giolga <giolga.kumi@gmail.com>
2024-08-12 18:44:59 +0400

    Models - Done

---
+++ README.md
@@ -0,0 +1,17 @@
+# TaskHub [200~In this course we will be creating a basic Task Management System called TaskHub wherein it caters the following basic functionalities.
+# TaskHub In this course I will be creating a basic Task Management System called TaskHub wherein it caters the following basic functionalities.
+
+Various system functionalities will exercise CRUD operation or Create, Read, Update and Delete which refers to the transactions done by the App to the Database.
+
+The app will have two roles Admin and User and both needs to Log-In first before using the system.
+
+Users can:
+
+View Tasks assigned to them
+Change the status of the task assigned to them
+Admins can:
+
+Create users
+Create tasks
+Delete tasks
+Edit task status
---
+++ TaskHub/Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskHub.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(255, ErrorMessage = "The FirstName should have a maximum of 255 characters!")]
+        [Display(Name = "First Name")] //Data Annotations
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(255, ErrorMessage = "The LastName should have a maximum of 255 characters!")]
+        public string LastName { get; set; }
+
+        public ICollection<UserTask> UserTasks { get; set; }
+        public  string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+    }
+}
